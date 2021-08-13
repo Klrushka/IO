3c1c1e8fc2e00762de75ec8474a109955a834195
Klrushka <comeindonotbeshy@gmail.com>
2021-08-13 14:49:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import task1.DirList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*
+        Exercise 1: (3) Modify DirList.java (or one of its variants) so that the FilenameFilter
+        opens and reads each file (using the net.mindview.util.TextFile utility) and accepts the
+        file based on whether any of the trailing arguments on the command line exist in that file.
+         */
+
+        DirList.start(args);
+
+
+
+        /*
+
+         */
+
+
+
+    }
+}
---
+++ src/task1/DirList.java
@@ -0,0 +1,33 @@
+package task1;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.regex.Pattern;
+
+public class DirList {
+    public static void start(String[] args) {
+        File path = new File(".");
+        final String[] list;
+        if (args.length < 2) {
+            list = path.list();
+        } else {
+            list = path.list(new FilenameFilter() {
+                private Pattern pattern = Pattern.compile(args[0]);
+
+                public boolean accept(File dir, String name) {
+                    return pattern.matcher(name).matches() &&
+                        !(Collections.disjoint(
+                            Arrays.asList(args),
+                            new TextFile(name)
+                        ));
+                }
+            });
+        }
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for (String dirItem : list)
+            System.out.println(dirItem);
+    }
+}
+
