192dd647db00e00b89c895d8f0e90476d14577d0
Klrushka <comeindonotbeshy@gmail.com>
2021-08-14 22:49:03 +0300

    <---- ^_^

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import task1.DirList;
+import task10.FReader;
 import task6.ProcessFiles;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -41,8 +48,37 @@ public class Main {
         System.out.println();
 
         /*
+        Exercise 7: (2) Open a text file so that you can read the file one line at a time. Read each
+        line as a String and place that String object into a LinkedList. Print all of the lines in the
+        LinkedList in reverse order.
+
+        Exercise 8: (1) Modify Exercise 7 so that the name of the file you read is provided as a
+        command-line argument.
+
+        Exercise 10: (2) Modify Exercise 8 to take additional command-line arguments of words
+        to find in the file. Print all lines in which any of the words match.
+         */
+
+        //E:\University\Programming\JavaDirectory\IO\src\testfiles\Test.txt
+
+        try {
+            System.out.println("Please enter file path" +
+                    "after enter words, which you need find through a space (If you want end, enter 0)");
+            FReader.readFileAndDisplayReverseOrder(new Scanner(System.in).nextLine(),FReader.readWords());
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+        System.out.println();
+
+        /*
 
          */
+
+
+
 
 
 
---
+++ src/task10/FReader.java
@@ -0,0 +1,54 @@
+package task10;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FReader {
+
+    public static void readFileAndDisplayReverseOrder(String path, LinkedHashSet<String> words) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        List<String> list = new LinkedList<>();
+        String tempString;
+        Pattern pattern = Pattern.compile(regex(words));
+        Matcher matcher = pattern.matcher("");
+
+
+        while ((tempString = bufferedReader.readLine()) != null) {
+            if (matcher.reset(tempString).find()) {
+                list.add(tempString);
+            }
+        }
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+
+    public static LinkedHashSet<String> readWords() {
+        Scanner scanner = new Scanner(System.in);
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        String temp;
+        while (!(temp = scanner.nextLine()).equals("0")) {
+            set.add(temp);
+        }
+        return set;
+
+    }
+
+    private static String regex(LinkedHashSet<String> set) {
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < set.size(); i++) {
+            if (i != set.size() - 1) {
+                buffer.append(".*").append(set.toArray()[i]).append(".*|");
+            } else buffer.append(".*").append(set.toArray()[i]).append(".*");
+        }
+        return buffer.toString();
+    }
+
+}
